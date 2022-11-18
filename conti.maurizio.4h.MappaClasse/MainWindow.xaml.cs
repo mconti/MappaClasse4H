bd5b4d7a2dd3f2df38cd234e22153912b381beb7
mconti <posta@maurizioconti.com>
2022-11-18 09:46:01 +0100

    Aggiunto event handler Utente per Tag

--- conti.maurizio.4h.MappaClasse/MainWindow.xaml.cs
+++ conti.maurizio.4h.MappaClasse/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using conti.maurizio._4h.MappaClasse.Models;
+
 namespace conti.maurizio._4h.MappaClasse
 {
     /// <summary>
@@ -23,13 +25,34 @@ namespace conti.maurizio._4h.MappaClasse
         public MainWindow()
         {
             InitializeComponent();
+            btn10.Tag = new Utente { Nome = "Maurizio", Inventario = "inv1234", Foto = "foto1.jpg" };
+            btn11.Tag = new Utente { Nome = "Giovanni", Inventario = "inv5678", Foto = "foto2.jpg" };
+            btn12.Tag = new Utente { Nome = "Daniele", Inventario = "inv91011", Foto = "foto3.jpg" };
+
         }
 
         private void OnClick(object sender, RoutedEventArgs e)
         {
-            Button b = sender as Button;
-
-            MessageBox.Show( "Hello World!" + b.Tag );
+            if (sender != null)
+            {
+                Button b = sender as Button;
+                if (b != null)
+                {
+                    if (b.Tag != null)
+                    {
+                        Utente u = b.Tag as Utente;
+                        MessageBox.Show($"Hello {u.Nome} {u.Inventario} {u.Foto}");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"qui Tag era null... ");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show($"qui sender era null... ");
+            }
         }
     }
 }
---
+++ conti.maurizio.4h.MappaClasse/Models/Utente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conti.maurizio._4h.MappaClasse.Models
+{
+    public class Utente
+    {
+        public string Nome { get; set; }
+        public string Inventario { get; set; }
+        public string Foto { get; set; }
+
+    }
+}
